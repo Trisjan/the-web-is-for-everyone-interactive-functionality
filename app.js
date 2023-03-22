ad25e6897a29257ab38df824f2b826fb9045cf8b
Trisjan <TrisjanM@hotmail.com>
2023-03-22 13:50:56 +0100

    detail page route fixed

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
 
 // API en de data fetchen
-const url = "https://api.visualthinking.fdnd.nl/api/v1/methods?first=100";
-const data = await fetch(url).then((response) => response.json());
+const baseurl = "https://api.visualthinking.fdnd.nl/api/v1/";
+const url = "methods?first=100";
+const data = await fetch(baseurl + url).then((response) => response.json());
 
 // Maak een nieuwe express app
 const app = express();
@@ -16,8 +17,19 @@ app.use(express.static("public"));
 
 // Maak een route voor de index
 app.get("/", function (req, res) {
-    res.render("index", data);
+  res.render("index", data);
 });
+
+app.get("/method/:slug", async function(req, res) {
+  
+  const slugUrl = req.params.slug || "/roadmap"
+  // console.log(baseurl + "method/" + slugUrl)
+
+  const data = await fetch(baseurl + "method/" + slugUrl).then((response) => response.json())
+  console.log(data)
+
+  res.render("method", data)
+})
 
 // Het poortnummer waarop de site lokaal gedraait wordt
 app.set("port", process.env.PORT || 8000);
