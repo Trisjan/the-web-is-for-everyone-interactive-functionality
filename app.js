e0dac01e7942012abd961c10873c2904497da448
Trisjan Mustamu <TrisjanM@hotmail.com>
2023-03-29 15:59:08 +0200

    still not working

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 import express from "express";
 
 // API en de data fetchen
-const baseurl = "https://api.visualthinking.fdnd.nl/api/v1/";
-const url = "methods?first=100";
-const data = await fetch(baseurl + url).then((response) => response.json());
+const url = "https://api.visualthinking.fdnd.nl/api/v1/";
 
 // Maak een nieuwe express app
 const app = express();
 
-// Stel ejs in als template engine en geef de views map door
+// Stel in hoe we express gebruiken
 app.set("view engine", "ejs");
 app.set("views", "./views");
-
-// Gebruikt de map 'public' voor statische resources
 app.use(express.static("public"));
 
 // Maak een route voor de index
-app.get("/", function (req, res) {
-  res.render("index", data);
+app.get("/", (request, response) => {
+  let methodsUrl = url + "/methods?first=100";
+  fetchJson(methodsUrl).then((data) => {
+    response.render("index", data);
+  });
 });
 
-app.get("/method/:slug", async function(req, res) {
-  
-  const slugUrl = req.params.slug || "/roadmap"
-  // console.log(baseurl + "method/" + slugUrl)
+// app.get("/method/:slug", async (request, response) => {
+//   const urlMethod = baseurl + 'method/' + request.params.slug
+//   // const slugUrl = request.params.slug || "/roadmap"
+//   // console.log(baseurl + "method/" + slugUrl)
+//   // const data = await fetch(baseurl + "method/" + slugUrl).then((response) => response.json())
+//   const data = await fetch(urlMethod).then((response) => response.json())
+//   console.log(data)
 
-  const data = await fetch(baseurl + "method/" + slugUrl).then((response) => response.json())
-  console.log(data)
+//   response.render("method", data)
+// })
 
-  res.render("method", data)
-})
+
+app.get("/method/:slug", (request, response) => {
+  let detailPageUrl = url + "method/" + request.params.slug;
+
+  fetchJson(detailPageUrl).then((data) => {
+    response.render("detail-page", data);
+  });
+});
 
 // Het poortnummer waarop de site lokaal gedraait wordt
 app.set("port", process.env.PORT || 8000);
@@ -39,3 +47,36 @@ app.listen(app.get("port"), function () {
     //
     console.log(`Application started on http://localhost:${app.get("port")}`)
 })
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
+}
+
+
+
+/**
+ * postJson() is a wrapper for the experimental node fetch api. It fetches the url
+ * passed as a parameter using the POST method and the value from the body paramater
+ * as a payload. It returns the response body parsed through json.
+ * @param {*} url the api endpoint to address
+ * @param {*} body the payload to send along
+ * @returns the json response from the api endpoint
+ */
+export async function postJson(url, body) {
+  return await fetch(url, {
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    .then((response) => response.json())
+    .catch((error) => error)
+}
